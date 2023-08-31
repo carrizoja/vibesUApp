d59d9a1d2e4b23c7d6194dea396e0a39626782d0
carrizoja@gmail.com <carrizoja@gmail.com>
2023-08-31 18:59:14 -0300

    Entrega Final V1.8

--- vibesU_project/settings.py
+++ vibesU_project/settings.py
@@ -49,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'vibesU_project.urls'
@@ -122,6 +124,7 @@ STATIC_URL = 'static/'
 import os
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
