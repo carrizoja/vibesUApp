f9c203bb255dcacf3dd4b5bd6c77a8d8fa8e9fb7
carrizoja@gmail.com <carrizoja@gmail.com>
2023-09-01 15:53:31 -0300

    Entrega Final V2.5

--- vibesU_project/settings.py
+++ vibesU_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     # my apps
     'AppVibesU',
 ]
@@ -52,8 +53,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     "django.middleware.security.SecurityMiddleware",
+    "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'vibesU_project.urls'
